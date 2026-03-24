e5e828e1e4eb34aa847435c03f88c5fe96da96f1
yoosufniyaf <yoosufniyaf40@gmail.com>
2026-03-24 22:15:21 +0500

    Update AuthContext.js

--- frontend/src/context/AuthContext.js
+++ frontend/src/context/AuthContext.js
@@ -1,5 +1,5 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { supabase } from "../supabase";
+import { supabase } from "../supabase.js";
 
 const AuthContext = createContext(null);
 
